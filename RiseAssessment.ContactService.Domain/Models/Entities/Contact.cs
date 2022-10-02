8473a068ffc108e1b0958875a1d5a4f1d5d2ba6f
Mustafa YAŞAR <mustafa@mustafayasar.info>
2022-10-03 01:36:15 +0300

    Add Entities

---
+++ RiseAssessment.ContactService.Domain/Models/Entities/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiseAssessment.ContactService.Domain.Models.Entities
+{
+    public class Contact : EntityBase<Guid>
+    {
+        public Contact(string name, string lastName, string company)
+        {
+            Name = name;
+            LastName = lastName;
+            Company = company;
+
+        }
+        public Contact()
+        {
+            ContactInfo = new List<ContactInformation>();
+
+        }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Company { get; set; }
+        public ICollection<ContactInformation> ContactInfo { get; set; }
+
+    }
+}
---
+++ RiseAssessment.ContactService.Domain/Models/Enums/InformationType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiseAssessment.ContactService.Domain.Models.Enums
+{
+    public enum InformationType
+    {
+        PhoneNumber = 1,
+        EmailAddress,
+        Location
+    }
+}
