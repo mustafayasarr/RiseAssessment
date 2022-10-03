e82571f71b0b8e29cfad29fd9d585c198272aef2
Mustafa YAŞAR <mustafa@mustafayasar.info>
2022-10-03 23:38:45 +0300

    add context

---
+++ RiseAssessment.ContactService.ContactAPI/Bootstrapper/ConfigurationExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RiseAssessment.ContactService.Infrastructure.Context;
+
+namespace RiseAssessment.ContactService.ContactAPI.Bootstrapper
+{
+    public static class ConfigurationExtension
+    {
+        public static IServiceCollection RegisterConfigurationServices(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddDbContext<ContactDbContext>(options =>
+                    options.UseNpgsql(Configuration.GetConnectionString("DevelopmentDbConnection")));
+        
+            return services;
+        }
+    }
+}
---
+++ RiseAssessment.ContactService.Infrastructure/Migrations/20221003203343_1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RiseAssessment.ContactService.Infrastructure.Migrations
+{
+    public partial class _1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contact",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Company = table.Column<string>(type: "text", nullable: false),
+                    CreatedAtUTC = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedAtUTC = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contact", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContactInformation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    InformationType = table.Column<int>(type: "integer", nullable: false),
+                    InformationContent = table.Column<string>(type: "text", nullable: true),
+                    ContactId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAtUTC = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedAtUTC = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactInformation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ContactInformation_Contact_ContactId",
+                        column: x => x.ContactId,
+                        principalTable: "Contact",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContactInformation_ContactId",
+                table: "ContactInformation",
+                column: "ContactId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContactInformation");
+
+            migrationBuilder.DropTable(
+                name: "Contact");
+        }
+    }
+}
